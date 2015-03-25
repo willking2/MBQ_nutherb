57c064a2748884ff68da927e517262bd74b7e114
Will King <will.king2@gmail.com>
2015-03-25 10:40:46 -0700

    residuals

--- analyses/MBQ_nutherb_analyses/ExpA_analysis.R
+++ analyses/MBQ_nutherb_analyses/ExpA_analysis.R
@@ -1,20 +1,34 @@
-## MBQ nutrients and herbivory analysis
-## Experiment A
-## GREX Moorea Winer 2014
+##### MBQ nutrients and herbivory analysis
+##### Experiment A
+##### GREX Moorea Winer 2014
 
-# name data table
+## name data table
 ExpA <- ExpA_grugosa_masses
 ExpA
 
-# remove two rows of missing data (lost OSW treatments)
+## remove two rows of missing data (lost OSW treatments)
 ExpA <- ExpA[-c(39,40), ]
 ExpA
 
-# box plots
-plot(chg_mass ~ trt, data = ExpA)
+## residuals
+r1 <- lm(ExpA$chg_mass ~ ExpA$trt)
+resid(r1)
+qqnorm(resid(r1))
+qqline(resid(r1))                       # qq plot of residuals appears leptokurtic
+# histogram
+hist(resid(r1))                         # histogram of residuals appears left skewed
+# residual vs. chg_mass
+plot(resid(r1) ~ ExpA$chg_mass)
 
-# Fligner-Killeen test of homogeneity of variances
-fligner.test(chg_mass ~ interaction(cage, sed_nut, watercol_nut), data = ExpA)
+
+## transform to equalize variances
+ExpA$trans_chg_mass <- 1/(ExpA$chg_mass)
+
+## box plots
+plot(trans_chg_mass ~ trt, data = ExpA)
+
+## Fligner-Killeen test of homogeneity of variances
+fligner.test(trans_chg_mass ~ interaction(cage, sed_nut, watercol_nut), data = ExpA)
 
 
 ################# bar plotting -- doesn't work yet -- #######################
